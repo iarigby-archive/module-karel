8804649ea80f07d985074bccae29617c69ff5da5
iarigby <marvinzem@gmail.com>
2020-04-25 01:40:16 +0400

    new configs

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import { ArgumentParser } from 'argparse'
 import { Partitions } from './partitions'
 import { RunOpts } from './runs'
 export const config = {
-    subject: 'შესავალი ციფრულ ტექნოლოგიებში'
+    subject: 'შესავალი ციფრულ ტექნოლოგიებში 2020'
 }
 export const env = {
     STUDENTS_DATA_PATH: `../../classroom-api/students.json`,
@@ -16,19 +16,21 @@ interface EnvOptions {
     download: boolean,
     runOpts: RunOpts
 }
-export function setEnv(): EnvOptions {
-    Object.entries(env).map(([k, v]) => process.env[k] = path.resolve(__dirname, v))
-    const parser = new ArgumentParser()
-    parser.addArgument(['-w', '--hw'])
-    parser.addArgument(['-s', '--slice'])
-    parser.addArgument(['-t', '--trial'])
-    parser.addArgument(['-d', '--download'])
-    parser.addArgument(['-e', '--restart'])
-    parser.addArgument(['-l', '--rerun'])
-    parser.addArgument(['-c', '--continue'])
-    parser.addArgument(['-o', '--omit'])
-    parser.addArgument(['-f', '--force'])
-    parser.addArgument(['-k', '--skip'])
+
+export function getArgs(): EnvOptions {
+    const parser = new ArgumentParser({
+        addHelp: true
+    })
+    parser.addArgument(['-w', '--hw'], {help: 'id of the homework'})
+    parser.addArgument(['-s', '--slice'], {help: 'check first n homeworks'})
+    parser.addArgument(['-t', '--trial'], {help: 'dont save output/print emails not send'})
+    parser.addArgument(['-d', '--download'], {help: 'whether to download or use existing file'})
+    parser.addArgument(['-e', '--restart'], {help: 'not working: delete all previous run data'})
+    parser.addArgument(['-l', '--rerun'], {help: 'not working: delete previous run data'})
+    parser.addArgument(['-c', '--continue'], {help: 'continue from userId'})
+    parser.addArgument(['-o', '--omit'], {help: 'skip all in category'})
+    parser.addArgument(['-f', '--force'], {help: 'force check of id'})
+    parser.addArgument(['-k', '--skip'], {help: 'skip check of id'})
     const args = parser.parseArgs()
     const hwId: string = args['hw']
 
@@ -75,6 +77,10 @@ export function setEnv(): EnvOptions {
         }
     }
 }
+export function setEnv(): EnvOptions {
+    Object.entries(env).map(([k, v]) => process.env[k] = path.resolve(__dirname, v))
+    return getArgs()
+}
 
 export function testerPath(hwId: string) {
     return path.resolve(__dirname, `../resources/${hwId}tester.js`)
@@ -93,6 +99,7 @@ export const homeworks: HwConfig[] = [
         name: 'დავალება 1',
         exceptions: {
             // late: ['gsamk19', 'aeris19', 'akuba19', 'ninchkh19', 'kpant19']
+            // late: ['atutb19']
         },
         // manualChecks: ['dtsom19', 'zkhut16']
         manualChecks: ['ninchkh19', 'aeris19']
@@ -104,6 +111,7 @@ export const homeworks: HwConfig[] = [
         manualChecks: ['rgoch19', 'lsats19', 'aatru19', 'apapi19'],
         exceptions: {
             // late: ['aerkv17']
+            // late: ['idoli19', 'atutb19']
         }
     },
     {
@@ -113,6 +121,7 @@ export const homeworks: HwConfig[] = [
         manualChecks: ['aatru19', 'tkhat19'],
         exceptions: {
             // late: ['nukapa19']
+            // late: ['idoli19', 'atutb19', 'lzoi19']
         }
     },
     {
@@ -128,5 +137,13 @@ export const homeworks: HwConfig[] = [
         id: 'hw3',
         name: 'დავალება 3',
         skip: ['gmode19']
+    },
+    {
+        id: 'hw4',
+        name: 'დავალება 4'
+    },
+    {
+        id: 'bonus-middle',
+        name: 'ბონუსი-შუა წერტილი (3%)'
     }
-]
+];
